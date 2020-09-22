a2e7ab2ce5504a6d333df6c74707381921fe9c0c
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2020-09-22 02:27:04 +0200

    Trigger Version 1.1
    1- Used Local Storage Feature to save the User Tasks in case of
    Reloading or close the App.
    2- Include Date and Time for each Created
    Task showing beside each Task.
    3- Update README File.

--- script.js
+++ script.js
@@ -1,60 +1,115 @@
-const addForm = document.querySelector('.add');
-const search = document.querySelector('.search input');
-const list = document.querySelector('.todos');
+const addForm   = document.querySelector('.add');
+const search    = document.querySelector('.search input');
+const list      = document.querySelector('.todos');
+let tasks       = [];
+let tasksDates  = [];
 
 addForm.add.focus();
 
-const generateTemplate = todo => {
-  const html = `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span>${todo}</span>
-      <i class="far fa-trash-alt delete"></i>
+//create and inject tasks into the UI
+const generateTemplate = () => {
+  let html = "";
+  for (let i in tasks, tasksDates) {
+    task  = tasks[i];
+    date  = tasksDates[i];
+    html +=
+    `<li class="list-group-item align-items-center" data-index="${i}">
+      <div class="d-flex justify-content-between">
+        <span>${task}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </div>
+      <div class="date text-muted" style="font-size: 12px">${date}</div>
     </li>
-  `;
-  list.innerHTML += html;
+    `;
+    //console.log(tasks);
+    //console.log(tasksDates)
+  }
+
+  list.innerHTML = html;
 };
 
-const filterTodos = term => {
-
-  // add filtered class
-  Array.from(list.children)
-    .filter(todo => !todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.add('filtered'));
-
-  // remove filtered class
-  Array.from(list.children)
-    .filter(todo => todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.remove('filtered'));
-
-};
-
-// add todos event
+// add tasks
 addForm.addEventListener('submit', e => {
-  
   e.preventDefault();
-  const todo = addForm.add.value.trim();
-
-  if(todo.length){
-    generateTemplate(todo);
+  const taskValue = addForm.add.value.trim();
+  const date      = new Date().toString().slice(0, -45);
+  if(taskValue.length){
+    tasks.push(taskValue);
+    tasksDates.push(date);
+    //console.log(tasks);
+    //console.log(tasksDates)
+    saveDataInStorage();
+    generateTemplate();
     addForm.reset();
   }
 
 });
 
-// delete todos event
+// delete tasks
 list.addEventListener('click', e => {
-
   if(e.target.classList.contains('delete')){
-    e.target.parentElement.remove();
+    e.target.parentElement.parentElement.remove();
+    tasks.splice(e.target.parentElement.parentElement.getAttribute("data-index"), 1);
+    tasksDates.splice(e.target.parentElement.parentElement.getAttribute("data-index"), 1);
+    //console.log(tasks);
+    //console.log(tasksDates);
+    saveDataInStorage();
     addForm.add.focus();
   }
 
 });
 
-// filter todos event
-search.addEventListener('keyup', () => {
+//filter tasks mechanism
+const filterTasks = term => {
+  // add filtered class
+  Array.from(list.children)
+    .filter(task  => !task.textContent.toLowerCase().includes(term))
+    .forEach(task => task.classList.add('filtered'));
 
+  // remove filtered class
+  Array.from(list.children)
+    .filter(task  => task.textContent.toLowerCase().includes(term))
+    .forEach(task => task.classList.remove('filtered'));
+
+};
+
+// filter tasks event
+search.addEventListener('keyup', () => {
   const term = search.value.trim().toLowerCase();
-  filterTodos(term);
+  filterTasks(term);
+});
+
+//prevent enter key default while searching
+search.addEventListener('keypress', e => {
+  let key = e.keyCode;
+  if (key == 13) {
+    e.preventDefault();
+  }  
 
 });
+
+//save data in local storage
+function saveDataInStorage() {
+  let tasksString  = JSON.stringify(tasks);
+  let datesString  = JSON.stringify(tasksDates);
+  localStorage.setItem("tasks", tasksString);
+  localStorage.setItem("tasks-dates", datesString);
+}
+
+//get tasks from local storage
+function getDataFromStorage() {
+  let tasksString = localStorage.getItem("tasks");
+  let datesString = localStorage.getItem("tasks-dates");  
+  tasks        = JSON.parse(tasksString);
+  tasksDates   = JSON.parse(datesString);
+  if(!tasks) { //if there is not any tasks stored in the local storage
+    tasks = [];
+  }
+  if(!tasksDates) { //if there is not any tasksDates stored in the local storage
+    tasksDates = [];
+  }
+}
+
+//initial functions when reloading page
+getDataFromStorage();
+generateTemplate();
